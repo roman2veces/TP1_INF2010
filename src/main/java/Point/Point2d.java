a8948be2878dce2680e2eb2d6eb392ad450dc6bd
roman2veces <romanroman2705@gmail.com>
2023-01-24 18:37:16 -0500

    initial commit

---
+++ src/main/java/Point/Point2d.java
@@ -0,0 +1,109 @@
+package Point;
+
+public class Point2d extends AbstractPoint {
+    private final Integer X = 0;
+    private final Integer Y = 1;
+
+    /** TODO
+     * 2D Point Constructor from coordinates
+     * @param x X coordinate
+     * @param y Y coordinate
+     */
+    public Point2d(Double x, Double y) {
+        super(new double[] {0.0, 0.0});
+    }
+
+    /** TODO
+     * 2D Point Constructor from vector
+     * @param vector Vector containing X and Y coordinates
+     */
+    public Point2d(Double[] vector) {
+        this(0.0, 0.0);
+    }
+
+    /**
+     * @return X coordinate
+     */
+    public double X() { return vector[X];}
+
+    /**
+     * @return Y coordinate
+     */
+    public double Y() { return vector[Y];}
+
+    /** TODO
+     * Translate the point by the given vector
+     * @param translateVector The vector by which to translate
+     * @return Translated point
+     */
+    @Override
+    public Point2d translate(Double[] translateVector) {
+        return null;
+    }
+
+    /** TODO
+     * Translate the point by the given 2D Point
+     * @param translateVector The 2D Point by which to translate
+     * @return Translated point
+     */
+    public Point2d translate(Point2d translateVector) {
+        return null;
+    }
+
+    /** TODO
+     * Rotate the point by the given rotation Matrix
+     * @param rotationMatrix Matrix by which to rotate
+     * @return Rotated point
+     */
+    @Override
+    public Point2d rotate(Double[][] rotationMatrix) {
+        return null;
+    }
+
+    /** TODO
+     * Rotate the point by the given angle
+     * @param angle Angle in radians
+     * @return Rotated point
+     */
+    public Point2d rotate(Double angle) {
+        return null;
+    }
+
+    /** TODO
+     * Divide the X and Y coordinates of a 2D point by a scalar
+     * @param divider Scalar used to divide
+     * @return Divided point
+     */
+    @Override
+    public Point2d divide(Double divider) {
+        return null;
+    }
+
+    /** TODO
+     * Multiply the X and Y coordinates of a 2D point by a scalar
+     * @param multiplier Scalar used to multiply
+     * @return Multiplied point
+     */
+    @Override
+    public Point2d multiply(Double multiplier) {
+        return null;
+    }
+
+    /** TODO
+     * Add a scalar to the X and Y coordinates of a 2D point
+     * @param adder Scalar to add
+     * @return Added point
+     */
+    @Override
+    public Point2d add(Double adder) {
+        return null;
+    }
+
+    /** TODO
+     * @return Deep copy of the 2D point
+     */
+    @Override
+    public Point2d clone() {
+        return null;
+    }
+}
---
+++ src/main/java/Point/PointOperator.java
@@ -0,0 +1,53 @@
+package Point;
+
+import java.util.*;
+
+public final class PointOperator {
+
+    /** TODO
+     * Translates a vector of size N by another vector of size N
+     * Similar to adding two vector of size N together
+     * @param vector Vector to translate
+     * @param translateVector Translation to apply
+     */
+    public static void translate(Double[] vector, Double[] translateVector) {
+
+    }
+
+    /** TODO
+     * Rotates a vector of size N by a matrix of size N x N
+     * Similar to a matrix vector multiplication
+     * @param vector Vector to rotate
+     * @param rotationMatrix Matrix by which to rotate
+     */
+    public static void rotate(Double[] vector, Double[][] rotationMatrix) {
+
+    }
+
+    /** TODO
+     * Divide a vector of size N by a scalar
+     * @param vector Vector to divide
+     * @param divider Scalar by which to divide
+     */
+    public static void divide(Double[] vector, Double divider) {
+
+    }
+
+    /** TODO
+     * Multiply a vector of size N by a scalar
+     * @param vector Vector to multiply
+     * @param multiplier Scalar by which to multiply
+     */
+    public static void multiply(Double[] vector, Double multiplier) {
+
+    }
+
+    /** TODO
+     * Add a scalar to a vector of size N
+     * @param vector Vector that we have to add to
+     * @param adder Scalar to add to vector
+     */
+    public static void add(Double[] vector, Double adder) {
+
+    }
+}
